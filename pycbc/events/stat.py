5a3c68cfe333f2d963adaf0e3734864dabf29c6e
Marton Tapai <marton.tapai@ligo.org>
2018-10-02 17:50:01 +0200

    adding the correct stats.py modification...

--- pycbc/events/stat.py
+++ pycbc/events/stat.py
@@ -133,12 +133,13 @@ class NewSNRStatistic(Stat):
         return (s0**2. + s1**2.) ** 0.5
 
 
-    def coinc_multiifo(self, slide, step, s=[]): # pylint:disable=unused-argument
+    def coinc_multiifo(self, s, slide, step,
+                       ): # pylint:disable=unused-argument
         """Calculate the coincident detection statistic.
         Parameters
         ----------
-        s[]: set of numpy.ndarray
-            Single detector ranking statistic for the detectors.
+        s: dictionary keyed by ifo of single detector ranking
+           statistics
         slide: (unused in this statistic)
         step: (unused in this statistic)
         Returns
@@ -146,8 +147,8 @@ class NewSNRStatistic(Stat):
         numpy.ndarray
             Array of coincident ranking statistic values
         """
-        newsnr = 0
-        for i in range(len(s)):
+        newsnr=0
+        for i in s:
             newsnr = newsnr + s[i]**2.
         return newsnr ** 0.5
 
